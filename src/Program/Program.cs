5756ac5a32b1f4e167e713341a09e5dc1660c0c5
PieroSaucedo <pierosaucedo@correo.ucu.edu.uy>
2021-09-15 19:36:02 -0300

    new file:   ../Library/Elfo.cs new file:   ../Library/Enano.cs new file:   ../Library/Item.cs new file:   ../Library/Mago.cs modified:   Program.cs

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -6,6 +6,15 @@ namespace Program
     {
         static void Main(string[] args)
         {
+        
+        item Casco = new Item("Yelmo de Hierro", 0, 30, 10); //Ataque, defensa, magia
+        item Chaleco = new Item("Manto de Cuero", 10, 50, 0);
+        item Chaleco = new Item("Armadura de Cota de Malla", 10, 25, 10);
+        item Botas = new Item("Grebas de Berserker"10, 10, 40);
+        item Guantes = new Item("Guanteletes de Fuego", 40, 5, 0);
+        item Guantes = new Item("Vendas de Tela", 35, 10, 5);
+        mago mago1 = new Mago("Ryze", 500, 0, 10)
+        
         }
     }
 }
