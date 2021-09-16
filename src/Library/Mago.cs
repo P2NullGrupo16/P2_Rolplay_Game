879db2ab12963dbb647ec3b617fcc087b02f7ae1
jumpert <juan.perezc@correo.ucu.edu.uy>
2021-09-16 12:42:11 -0300

    Juego Terminado

--- src/Library/Mago.cs
+++ src/Library/Mago.cs
@@ -83,16 +83,17 @@ namespace Program
                 this.mano1 = value;
                 this.Magia += value.Magia;
                 this.Defensa += value.Defensa;
-                this.Magia += value.Magia;
+                this.Ataque += value.Ataque;
             }
         }
-        //public Item Mano2 {get; set;}
+        //public Item Mano2 {get; set;}   El mago, no tiene habilitada esta mano, ya que tiene el libro de hechizos
         
         public double Defensa {get; set;}
         public double Ataque {get;set;}
         public string Nombre {get;set;}
         public double Resistencia {get; set;}
         public double Vida1 = 120;
+        public double Magia {get;set;}
         
         public double Armadura
         {
@@ -116,16 +117,16 @@ namespace Program
                 this.vida = value;
             }
         }
-        public double Magia {get;set;}
+        
         
         public double AtaqueTotal() 
         {
-            double ataqueTotal = this.Ataque + this.Mano1.Ataque + this.LibroHechizosPoder();
+            double ataqueTotal = this.Ataque + this.LibroHechizosPoder();
             return ataqueTotal;
         }
         public double DefensaTotal()
         {
-            double defensaTotal = this.Defensa + this.Mano1.Defensa +  this.Armadura;
+            double defensaTotal = this.Defensa +  this.Armadura;
             return defensaTotal;            
         }
         public void AtacarMago(Mago otro_pj)
@@ -133,13 +134,14 @@ namespace Program
             this.Resistencia -= 10;
             this.Magia -= 15;
             otro_pj.Vida -= this.AtaqueTotal();
-            
+            Console.WriteLine($"Se restaron {this.AtaqueTotal()} puntos a la vida de {otro_pj.Nombre}");
         }
         public void AtacarEnano(Enano otro_pj)
         {   
             this.Resistencia -= 10;
             this.Magia -= 15;
             otro_pj.Vida -= this.AtaqueTotal();
+            Console.WriteLine($"Se restaron {this.AtaqueTotal()} puntos a la vida de {otro_pj.Nombre}");
             
         }
         public void AtacarElfo(Elfo otro_pj)
@@ -147,7 +149,7 @@ namespace Program
             this.Resistencia -= 10;
             this.Magia -= 15;
             otro_pj.Vida -= this.AtaqueTotal();
-            
+            Console.WriteLine($"Se restaron {this.AtaqueTotal()} puntos a la vida de {otro_pj.Nombre}");
         }
         public void EstudiarHechizo(string hechizo)
         {
@@ -172,10 +174,19 @@ namespace Program
         }
         public void RemoveCasco()
         {
-            this.Casco = new Item("",0,0,0);
+            if (!(this.Casco.Nombre == ""))
+            {
+                this.Defensa -= this.Casco.Defensa;
+                this.Casco = new Item("",0,0,0);
+            }
+            else
+            {
+                Console.WriteLine("No tiene objeto equipado en Casco");
+            }
         }
         public void ChangeCasco (Item casco)
         {
+            this.RemoveCasco();
             this.Casco = casco;
         }
         public void AddChaleco(Item chaleco)
@@ -184,10 +195,19 @@ namespace Program
         }
         public void RemoveChaleco()
         {
-            this.Chaleco = new Item("",0,0,0);
+            if (!(this.Chaleco.Nombre == ""))
+            {
+                this.Defensa -= this.Chaleco.Defensa;
+                this.Chaleco = new Item("",0,0,0);
+            }
+            else
+            {
+                Console.WriteLine("No tiene objeto equipado en Chaleco");
+            }
         }
         public void ChangeChaleco (Item chaleco)
         {
+            this.RemoveChaleco();
             this.Chaleco = chaleco;
         }
         public void AddBotas(Item botas)
@@ -196,10 +216,19 @@ namespace Program
         }
         public void RemoveBotas()
         {
-            this.Botas = new Item("",0,0,0);
+            if (!(this.Botas.Nombre == ""))
+            {
+                this.Defensa -= this.Botas.Defensa;
+                this.Botas = new Item("",0,0,0);
+            }
+            else
+            {
+                Console.WriteLine("No tiene objeto equipado en Botas");
+            }
         }
         public void ChangeBotas (Item botas)
         {
+            this.RemoveBotas();
             this.Botas = botas;
         }
         public void AddMano1(Item baculo)
@@ -208,11 +237,22 @@ namespace Program
         }
         public void RemoveMano1()
         {
-            this.Mano1 = new Item("",0,0,0);
+            if (!(this.Mano1.Nombre == ""))
+            {
+                this.Magia -= this.Mano1.Magia;
+                this.Defensa -= this.Mano1.Defensa;
+                this.Ataque -= this.Mano1.Ataque;
+                this.Mano1 = new Item("",0,0,0);
+            }
+            else
+            {
+                Console.WriteLine("No tiene objeto equipado en Mano1");
+            }
         }
-        public void ChangeMano1 (Item baculo)
+        public void ChangeMano1 (Item armaNuevo)
         {
-            this.Mano1 = baculo;
+            this.RemoveMano1();
+            this.Mano1 = armaNuevo;
         }
 
 
--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -10,35 +10,45 @@ namespace Program
             Mago pj1 = new Mago("Juan");
             Item gorro = new Item("Gorro magico", 0, 20,0 );
             Item baculo = new Item("Baculo", 10,10,10);
+            Item baston = new Item("Baston", 10,10,15);
             Item escudo = new Item("Escudo divino", 10,1000,10);
             pj1.AddMano1(baculo);
-            //Console.WriteLine(pj1.Mano1.GetItem());
+            Console.WriteLine(pj1.Mano1.GetItem());
             pj1.AddCasco(gorro);
-            //Console.WriteLine(pj1.LibroHechizosPoder());
-            //Console.WriteLine(pj1.Vida);
+            Console.WriteLine(pj1.LibroHechizosPoder());
+            Console.WriteLine(pj1.Vida);
             pj1.EstudiarHechizo("Lluvia acida");
-            //Console.WriteLine(pj1.LibroHechizosPoder());
-            //Console.WriteLine(pj1.Ataque);
-            //Console.WriteLine(pj1.Mano1.Ataque);
-            Console.WriteLine(pj1.DefensaTotal() + " ataque total");
-            //Console.WriteLine(pj1.Ataque);
-            //Console.WriteLine(pj1.VerLibroHechizos());
+            Console.WriteLine(pj1.LibroHechizosPoder());
+            Console.WriteLine(pj1.Ataque);
+            Console.WriteLine(pj1.Mano1.Ataque);
+            Console.WriteLine(pj1.AtaqueTotal() + " ataque total");
+            Console.WriteLine(pj1.Ataque);
+            Console.WriteLine(pj1.VerLibroHechizos());
             pj1.OlvidarHechizo("Lluvia acida");
-            //Console.WriteLine(pj1.VerLibroHechizos());
-            Console.WriteLine(pj1.Magia);
+            Console.WriteLine(pj1.VerLibroHechizos());
+            Console.WriteLine(pj1.Magia + " magia con baculo");
+            pj1.RemoveMano1();
+            pj1.ChangeMano1(baston);
+            Console.WriteLine(pj1.Magia + " magia sin baculo");
+
+
             
 
             Elfo pj2 = new Elfo("Juan");
             Item mascara = new Item("Mascara", 0, 20,0 );
-            Item arco = new Item("Arco", 30,10,10);
+            Item arco = new Item("Arco Maestro", 30,10,10);
             Item flecha = new Item("Flecha roja", 5,0,0);
             pj2.AddMano1(arco);
             pj2.AddMano2(flecha);
-            //Console.WriteLine(pj1.Mano1.GetItem());
+            Console.WriteLine(pj1.Mano1.GetItem());
             pj2.AddCasco(mascara);
+            pj2.AtacarMago(pj1);
+            Console.WriteLine($"{pj1.Vida} puntos de vida");
+            pj2.CurarMago(pj1);
+            
             Console.WriteLine(pj2.Ataque);
-            Console.WriteLine(pj2.Armadura);
-
+            Console.WriteLine(pj2.DefensaTotal() + " defensa total");
+            Console.WriteLine(pj2.AtaqueTotal() + " ataque total");
         }
     }
 }
